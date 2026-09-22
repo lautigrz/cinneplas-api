0e237ce1c7ccd98f7c33ea18d8325398b3d77c51
lautigrz <lautarogerezz12@gmail.com>
2026-09-22 14:56:08 -0300

    new migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AuthModule } from './auth/AuthModule.js';
 import { PrismaModule } from './prisma/PrismaModule.js';
 import { CinemaModule } from './cinema/CinemaModule.js';
 import { CinemaRoomModule } from './cinema-room/CinemaRoomModule.js';
+import { AwsModule } from './aws/AwsModule.js';
+import { MovieModule } from './movie/MovieModule.js';
+import { TmdbModule } from './tmdb/TmdbModule.js';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CinemaModule, CinemaRoomModule]
+  imports: [PrismaModule, AuthModule, CinemaModule, CinemaRoomModule, AwsModule, MovieModule, TmdbModule]
 })
 export class AppModule { }
 
